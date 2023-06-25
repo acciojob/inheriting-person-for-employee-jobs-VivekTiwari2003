9d1fb4e0ab61eebab3e41ae37f9f34bd8849e3f6
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-06-25 10:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,27 +8,28 @@
 // window.Employee = Employee;
 
 // Person constructor function
+// complete this js code
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+	this._name=name;
+	this._age=age;
+}
+Person.prototype.greet=function(){
+	console.log("Hello, my name is "+this._name+" and I am "+this._age+" years old.")
 }
 
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+	Person.call(this,name,age)
+	this.jobTitle= jobTitle
 }
 
-Person.prototype.greet = function() {
-  console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
-};
+Employee.prototype = Object.create(Person.prototype)
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+Employee.prototype.constructor= Employee
+Employee.prototype.jobGreet = function (){
+	console.log("Hello, my name is "+this._name+", I am "+this._age+" years old, and my job title is "+this.jobTitle+".")
+}
 
-Employee.prototype.jobGreet = function() {
-  console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
-};
-
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
 
